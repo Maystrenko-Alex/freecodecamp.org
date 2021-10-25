c1f3f84d32fe05bdf15130c9ef5c835289c15f42
Maystrenko-Alex <93150928+Maystrenko-Alex@users.noreply.github.com>
2021-10-25 23:47:51 +0300

    Create 36.js

---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/36.js
@@ -0,0 +1,8 @@
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = "The " + myNoun + " a " + myAdjective + ", and she " + myVerb + " is very " + myAdverb + "."; // Change this line
+// Only change code above this line
