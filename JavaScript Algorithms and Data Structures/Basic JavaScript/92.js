53fdb479e485a3c985c138843bf65f7607848bf8
Maystrenko-Alex <93150928+Maystrenko-Alex@users.noreply.github.com>
2021-10-26 22:24:58 +0300

    Create 92.js

---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/92.js
@@ -0,0 +1,24 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+   "artist": "Brayan Adams",
+    "title": "InnerView",
+    "release_year": 1983,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true 
+  }
+];
