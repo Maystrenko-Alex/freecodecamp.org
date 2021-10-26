00dd553419e5bc138c099baf4ea61cfa96dfd764
Maystrenko-Alex <93150928+Maystrenko-Alex@users.noreply.github.com>
2021-10-26 22:28:23 +0300

    Create 93.js

---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/93.js
@@ -0,0 +1,14 @@
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = undefined;
+gloveBoxContents = myStorage.car.inside["glove box"];
